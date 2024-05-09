184103197a5e30bfb45a8e934a96de1b467b6344
Jean Prunneaux <jean@prunneaux.com>
2024-05-09 10:07:08 +0200

    Integrate Bootstrap into project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
+import { createApp } from "vue"
 // import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
-import api from './plugins/api.js'
-import globalvars from './plugins/global.js'
-import i18n from './plugins/i18n.js'
+import App from "./App.vue"
+import router from "./router"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+import api from "./plugins/api.js"
+import globalvars from "./plugins/global.js"
+import i18n from "./plugins/i18n.js"
 
-import './assets/main.css'
+import "./assets/main.css"
 
 const app = createApp(App)
 
@@ -17,4 +19,4 @@ app.use(globalvars)
 app.use(i18n)
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")

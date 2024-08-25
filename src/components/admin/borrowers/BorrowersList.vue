45754b76738f81d1fe9cb47646dd8f9f644b4c1d
Jean Prunneaux <jean@prunneaux.com>
2024-08-25 23:23:07 +0200

    Add image if empty borrowers

--- src/components/admin/borrowers/BorrowersList.vue
+++ src/components/admin/borrowers/BorrowersList.vue
@@ -8,58 +8,65 @@
       </router-link>
 
       <Loading v-if="loading" />
-
-      <template v-if="borrowers && borrowers.length > 0">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">{{ $t("Firstname") }}/{{ $t("Lastname") }}</th>
-              <th scope="col">{{ $t("Email") }}</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(borrower, key) in borrowers" :key="key">
-              <td scope="row">
-                <a :href="borrower.id + '/'">
-                  {{ borrower.firstname }} {{ borrower.lastname }}
-                </a>
-              </td>
-              <td scope="row">
-                <a :href="borrower.id + '/'">{{ borrower.email }}</a>
-              </td>
-              <td>
-                <router-link
-                  v-if="$matchUserId(borrower.owner)"
-                  :to="'' + borrower.id + '/'"
-                  :title="$t('View')"
-                  class="me-3"
-                >
-                  <i class="bi bi-eye" />
-                </router-link>
-                <router-link
-                  v-if="$matchUserId(borrower.owner)"
-                  :to="'' + borrower.id + '/edit'"
-                  :title="$t('Edit')"
-                  class="me-3"
-                >
-                  <i class="bi bi-pencil" />
-                </router-link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <template v-else>
+        <template v-if="borrowers && borrowers.length > 0">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">{{ $t("Firstname") }}/{{ $t("Lastname") }}</th>
+                <th scope="col">{{ $t("Email") }}</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(borrower, key) in borrowers" :key="key">
+                <td scope="row">
+                  <a :href="borrower.id + '/'">
+                    {{ borrower.firstname }} {{ borrower.lastname }}
+                  </a>
+                </td>
+                <td scope="row">
+                  <a :href="borrower.id + '/'">{{ borrower.email }}</a>
+                </td>
+                <td>
+                  <router-link
+                    v-if="$matchUserId(borrower.owner)"
+                    :to="'' + borrower.id + '/'"
+                    :title="$t('View')"
+                    class="me-3"
+                  >
+                    <i class="bi bi-eye" />
+                  </router-link>
+                  <router-link
+                    v-if="$matchUserId(borrower.owner)"
+                    :to="'' + borrower.id + '/edit'"
+                    :title="$t('Edit')"
+                    class="me-3"
+                  >
+                    <i class="bi bi-pencil" />
+                  </router-link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </template>
+        <Empty
+          v-else
+          :label="$t('No borrowers') + ' ' + $t('Create first borrower')"
+        />
       </template>
     </div>
   </div>
 </template>
 
 <script>
+import Empty from "@/components/Empty.vue"
 import Error from "@/components/Error.vue"
 import Loading from "@/components/Loading.vue"
 
 export default {
   components: {
+    Empty,
     Error,
     Loading,
   },
